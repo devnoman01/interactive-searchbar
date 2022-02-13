57c5af2dc974e5787b7ca29bef56ad2fdce64583
devnoman01 <devnoman01@gmail.com>
2022-02-13 14:50:25 +0600

    Email search working properly

--- js/script.js
+++ js/script.js
@@ -1,35 +1,78 @@
-const allNames = document.querySelectorAll('.person-name');
-var personNames = [];
-for(const name of allNames){
-    personNames.push(name.innerHTML);
+let resultName = "";
+let resultEmail = "";
+
+const personInfo = [
+    {name: "Abdul Hadi", email: "183003022e@eastdelta.edu.bd"},
+    {name: "Abdullah Al Noman", email: "183002922e@eastdelta.edu.bd"},
+    {name: "Abdullah Imran Hriday", email: "183001622e@eastdelta.edu.bd"},
+    {name: "Abu Hanif", email: "183002422e@eastdelta.edu.bd"},
+    {name: "Anjan Chakraborty", email: "183002622e@eastdelta.edu.bd"},
+    {name: "Arafat Hossain", email: "183001722e@eastdelta.edu.bd"},
+    {name: "Ariful Islam", email: "183000222e@eastdelta.edu.bd"},
+    {name: "Asif Iqbal Palash", email: "183000822e@eastdelta.edu.bd"},
+    {name: "Faruq Hasan", email: "183002022e@eastdelta.edu.bd"},
+    {name: "Farzana Smrity", email: "183002122e@eastdelta.edu.bd"},
+    {name: "Kashpiya Amina", email: "183000422e@eastdelta.edu.bd"},
+    {name: "Monsurul Alam", email: "183002522e@eastdelta.edu.bd"},
+    {name: "Najnin Sultana", email: "183000522e@eastdelta.edu.bd"},
+    {name: "Nurun Nabi", email: "183002722e@eastdelta.edu.bd"},
+    {name: "Shagatam Mutsuddy", email: "183001422e@eastdelta.edu.bd"},
+    {name: "Shahariar Shaown", email: "183002322e@eastdelta.edu.bd"},
+    {name: "Zainal Abedin", email: "183000322e@eastdelta.edu.bd"},
+];
+
+const personContainer = document.getElementById("person-container");
+
+function loadUser(name, email) {
+    const personDetails = document.createElement("div");
+    personDetails.classList.add("col-xl-3", "col-lg-4", "col-md-6", "col-sm-12");
+    personDetails.innerHTML = `
+    <div class="person shadow rounded rounded-2 p-3 my-3">
+        <div class="row">
+            <h5 class="person-name">${name}</h5>
+            <p class="person-email">${email}</p>
+        </div>
+        <div class="location-info">
+            <small>Chittagong</small>
+            <small>Now: Chittagong</small>
+        </div>
+    </div>
+    `;
+    personContainer.appendChild(personDetails);
 }
-const allEmails = document.querySelectorAll('.person-email');
-var personEmails = [];
-for(const email of allEmails){
-    personEmails.push(email.innerHTML);
+
+function loadNoUser() {
+    const personDetails = document.createElement("div");
+    personContainer.innerHTML = ``;
+    personContainer.appendChild(personDetails);
+}
+
+function loadAllUser() {
+    for(var person of personInfo ){
+        userName = person.name;
+        userEmail = person.email
+        loadUser(userName, userEmail);
+    }
 }
 
 
 function searchPerson() {
-    var searchValue = document.getElementById('searchText').value;
-    // console.log(searchValue);
-    for(let i=0; i<personNames.length;i++){
-        
-        if(personNames[i].includes(searchValue, 0)){
-            if(searchValue!=''){
-                console.log(personNames[i]);
+    
+    var searchValue = document.getElementById("searchText").value;
+    if(searchValue == ""){
+        loadAllUser();
+    }
+    else{
+        loadNoUser()
+        for(let i=0; i<personInfo.length; i++){
+
+            if(personInfo[i].name.includes(searchValue) || personInfo[i].email.includes(searchValue)){
+                resultName = personInfo[i].name;
+                resultEmail = personInfo[i].email;
+                loadUser(resultName, resultEmail);
             }
-        
         }
-        else{
-            continue;
-        }
-        // console.log(personNames[i]);
-        // if(i==5){
-        //     break;
-        // }
-        // if(name.includes(searchValue)){
-        //     console.log('found name');
-        // }
     }
 }
+
+document.onload = loadAllUser();
